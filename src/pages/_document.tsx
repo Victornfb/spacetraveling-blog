ff79c1238efb3b3c7dfde8f130d750164e2fa084
Victor Nunes <victornfb@outlook.com>
2022-01-18 13:26:32 -0300

    Style home

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,21 @@
-import Document from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +27,59 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Home | spacetraveling</title>
+    </Head>
+    <main className={styles.contentContainer}>
+      <section>
+        <img className={styles.logo} src="/images/Logo.svg" alt="logo" />
+        <div className={styles.posts}>
+          <Link href="/post/1.tsx">
+            <a>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.info}>
+                <span className={styles.infoIcon}><FiCalendar /></span>
+                <time>15 Mar 2021</time>
+                <span className={styles.infoIcon}><FiUser /></span>
+                <span>Joseph Oliveira</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="/post/1.tsx">
+            <a>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.info}>
+                <span className={styles.infoIcon}><FiCalendar /></span>
+                <time>15 Mar 2021</time>
+                <span className={styles.infoIcon}><FiUser /></span>
+                <span>Joseph Oliveira</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="/post/1.tsx">
+            <a>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.info}>
+                <span className={styles.infoIcon}><FiCalendar /></span>
+                <time>15 Mar 2021</time>
+                <span className={styles.infoIcon}><FiUser /></span>
+                <span>Joseph Oliveira</span>
+              </div>
+            </a>
+          </Link>
+        </div>
+        <button className={styles.loadButton}>Carregar mais posts</button>
+      </section>
+    </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
